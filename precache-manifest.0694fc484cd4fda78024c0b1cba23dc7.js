6a168b55a700c86d358477f82bef370a30935e14
AmineB <amine.benh95@gmail.com>
2018-12-04 12:10:21 +0000

    Updates

---
+++ precache-manifest.0694fc484cd4fda78024c0b1cba23dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "bd4c7886bf83a35cfaaf",
+    "url": "/contactmanager/static/js/main.bd4c7886.chunk.js"
+  },
+  {
+    "revision": "cf6a5219a8325ad3a5d1",
+    "url": "/contactmanager/static/js/1.cf6a5219.chunk.js"
+  },
+  {
+    "revision": "bd4c7886bf83a35cfaaf",
+    "url": "/contactmanager/static/css/main.499c6569.chunk.css"
+  },
+  {
+    "revision": "cf6a5219a8325ad3a5d1",
+    "url": "/contactmanager/static/css/1.6a069fce.chunk.css"
+  },
+  {
+    "revision": "09b10396b79f17fea5b0f12bd4cadecf",
+    "url": "/contactmanager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/abenH/contactmanager.git/precache-manifest.22a27bdbb60c22a67187e9da80b14748.js"
+  "/contactmanager/precache-manifest.0694fc484cd4fda78024c0b1cba23dc7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/abenH/contactmanager.git/index.html", {
+workbox.routing.registerNavigationRoute("/contactmanager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
